7445ea9311c5d23ed7a08df1bd55179a5e15f0d3
Maxwell Cohen <to.email.max@gmail.com>
2026-04-10 22:31:53 -0500

    Update favicon link in entry-server.tsx to use SVG format for better scalability and performance.

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -11,7 +11,7 @@ export default createHandler(() => (
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="referrer" content="strict-origin-when-cross-origin" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
           <link
